ef8522f420fd8f8a4937602e988993f38aeede91
Bastian1110 <chebimora02@gmail.com>
2022-11-10 19:01:04 -0600

    Revision 1

--- MultiAgent/car.py
+++ MultiAgent/car.py
@@ -3,20 +3,23 @@ from mesa import Agent
 
 class Car(Agent):
     MOVE = 1
+    DIRECTIONS = {"N": [0, 1], "E": [1, 0], "S": [0, -1], "W": [1, 0]}
 
-    def __init__(self, pos, model, rules, init_state=MOVE):
+    def __init__(self, pos, model, init_state=MOVE):
         super().__init__(pos, model)
         self.x, self.y = pos
         self.state = init_state
-        self._nextState = None
 
-    def move(self):
+    def move(self, direction):
         newx, newy = self.pos
-        self.model.grid.move_agent(self, (newx + 1, newy + 1))
+        self.model.grid.move_agent(
+            self,
+            (
+                newx + self.DIRECTIONS[direction][0],
+                newy + self.DIRECTIONS[direction][1],
+            ),
+        )
 
     def step(self):
         self._nextState = self.state
-        self.move()
-
-    def advance(self):
-        self.state = self._nextState
+        self.move("S")
--- MultiAgent/portrayal.py
+++ MultiAgent/portrayal.py
@@ -1,26 +1,12 @@
-def portrayalCar(car):
-    assert car is not None
+def portrayal(agent):
+    assert agent is not None
     return {
         "Shape": "rect",
         "w": 1,
         "h": 1,
         "Filled": "true",
         "Layer": 0,
-        "x": car.x,
-        "y": car.y,
-        "Color": "black",
-    }
-
-
-def portrayalObstacle(obstacle):
-    assert obstacle is not None
-    return {
-        "Shape": "rect",
-        "w": obstacle.w,
-        "h": obstacle.h,
-        "Filled": "true",
-        "Layer": 0,
-        "x": obstacle.x,
-        "y": obstacle.y,
-        "Color": "blue",
+        "x": agent.x,
+        "y": agent.y,
+        "Color": "red",
     }
