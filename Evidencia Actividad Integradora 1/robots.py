b31afd1cf0d4883c4beeca39f87c45635254723e
Adrian101-hnd <69586718+Adrian101-hnd@users.noreply.github.com>
2022-11-16 15:39:03 -0600

    Documentacion Agente y Modelo

---
+++ Evidencia Actividad Integradora 1/robots.py
@@ -0,0 +1,28 @@
+"""
+Script de Agentes, Robot y Caja
+Modelo del Medio Ambiente
+
+"""
+
+
+from mesa import Agent, Model
+from mesa.time import RandomActivation
+from mesa.space import Grid
+
+class Robot(Agent):
+    """
+    Este es un robot, el robot puede moverse en las cuatro dirrecciones, puede levantar cajas 
+    adjacentes en cuadricula y llevarcelas para hacer pilas de 5 cajas, su vision esta limitada a
+    las cuatro celdas adjacentes en cuadricula lo cual significa que puede saber si un espacio esta
+    ocupado o libre, finalmente el robot tambien puede saber si trae una caja.
+    """
+
+class Caja(Agent):
+    """
+    Es una caja. Puede ser movida por un robot.
+    """
+
+class Enviorment(Model):
+    """
+    Crea un ambiente con 5 robots y un numero 'X' de cajas.
+    """
