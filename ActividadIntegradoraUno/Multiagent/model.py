99ae66c43b04bd5ecb4154b4b74f71ebc6b9e47f
Bastian1110 <chebimora02@gmail.com>
2022-11-20 12:20:57 -0600

    Starting web server complete

--- ActividadIntegradoraUno/Multiagent/model.py
+++ ActividadIntegradoraUno/Multiagent/model.py
@@ -1,5 +1,5 @@
 from mesa import Model
-from mesa.time import RandomActivation
+from mesa.time import SimultaneousActivation
 from mesa.space import MultiGrid
 from box import Box
 from robot import Robot
@@ -10,12 +10,13 @@ class robotModel(Model):
         super().__init__()
         self.numcajas = nCajas
         self.grid = MultiGrid(width, height, False)
-        self.schedule = RandomActivation(self)
+        self.schedule = SimultaneousActivation(self)
         self.running = True
         self.actualStorage = (0,0)
         self.StorageList = []
         self.StorageList.append(self.actualStorage)
         self.index = 0
+        self.running = True 
 
         #Crear robots
         for i in range(nRobots):
@@ -28,7 +29,7 @@ class robotModel(Model):
 
         for i in lugarCajas:
             if self.grid.is_cell_empty((i[1],i[2])):
-                caja = Box((i[1],i[2]), self,show=True)
+                caja = Box(self.next_id(),(i[1],i[2]), self,show=True)
 
                 self.grid.place_agent(caja, (i[1],i[2]))
 
--- ActividadIntegradoraUno/Multiagent/webSever.py
+++ ActividadIntegradoraUno/Multiagent/webSever.py
@@ -4,58 +4,66 @@ from model import robotModel
 from robot import Robot
 from box import Box
 
+
+numberRobots = 2
+numberBoxes = 10
 width = 10
 height = 10
-model = None
+modelo = None
 currentStep = 0
 
 app = Flask("Robots vs Boxes")
 
 @app.route('/init', methods=['POST', 'GET'])
 def initModel():
-    global currentStep, model, number_agents, width, height
+    global currentStep, modelo, numberRobots, numberBoxes, width, height
 
     if request.method == 'POST':
-        print(request.form)
-        numberRobots = int(request.form.get('NRobots'))
-        numberBoxes = int(request.form.get('NBoxes'))
-        width = int(request.form.get('width'))
-        height = int(request.form.get('height'))
+        request_data = request.get_json()
+        print(request_data)
+        numberRobots = int(request_data["NRobots"])
+        numberBoxes = int(request_data["NBoxes"])
+        width = int(request_data["width"])
+        height = int(request_data["height"])
         currentStep = 0
 
-        print(request.form)
-        print(number, width, height)
-        model = robotModel(numberBoxes, numberRobots, width, height)
+        modelo = robotModel(numberBoxes, numberRobots, width, height)
 
-        return jsonify({"message":"Parameters recieved, model initiated."})
-    if request.method == 'GET':
         return jsonify({"message":"Parameters recieved, model initiated."})
 
 @app.route('/getRobots', methods=['GET'])
 def getRobots():
-    global model
+    global modelo
 
     if request.method == 'GET':
-        robotPositions = [{"id": str(a.unique_id), "x": x, "y":1, "z":z} for (a, x, z) in model.grid.coord_iter() if isinstance(a, Robot)]
+        robotPositions = []
+        for (a, x, z) in modelo.grid.coord_iter():
+            for o in a:
+                if isinstance(o, Robot):
+                    robotPositions.append({"id": str(o.unique_id), "x": x, "y":1, "z":z})
 
         return jsonify({'positions': robotPositions})
 
 @app.route('/getBoxes', methods=['GET'])
 def getBoxes():
-    global model
+    global modelo
 
     if request.method == 'GET':
-        boxPositions = [{"id": str(a.unique_id), "x": x, "y":1, "z":z} for (a, x, z) in model.grid.coord_iter() if isinstance(a, Box)]
+        boxPositions = []
+        for (a, x, z) in modelo.grid.coord_iter():
+            for o in a:
+                if isinstance(o, Box):
+                    boxPositions.append({"id": str(o.unique_id), "x": x, "y":1, "z":z})
 
         return jsonify({'positions': boxPositions})
 
 @app.route('/update', methods=['GET'])
 def updateModel():
-    global currentStep, model
+    global currentStep, modelo
     if request.method == 'GET':
-        model.step()
+        modelo.step()
         currentStep += 1
         return jsonify({'message':f'Model updated to step {currentStep}.', 'currentStep':currentStep})
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=8585, debug=True)
+    app.run(host="localhost", port=8080)
