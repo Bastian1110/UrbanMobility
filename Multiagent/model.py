5f53a2e468ff75df23cfb1f616f447c5864fb826
Bastian1110 <chebimora02@gmail.com>
2022-11-29 12:03:48 -0600

    Little fix in model

--- Multiagent/model.py
+++ Multiagent/model.py
@@ -30,7 +30,7 @@ class UrbanMobility(Model):
 
             for r, row in enumerate(lines):
                 for c, col in enumerate(row):
-                    if col in ["v", "^", ">", "<"]:
+                    if col in list("<>^v"):
                         agent = Road(
                             f"r_{r*self.width+c}", self, "f", dataDictionary[col]
                         )
